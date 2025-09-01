7caffe17f4fe6d2b58bdf5c2c5019fc4c2d9718a
Jeans <sistemadesignstyle@gmail.com>
2025-08-31 23:08:45 -0500

    modificando environment

---
+++ dist/proy_test/browser/chunk-6WUTALT2.js
@@ -0,0 +1,1 @@
+var t={production:!0,urlDominioApi:"https://apitienda.smartdigitaltec.com",urlRaiz:"https://apitienda.smartdigitaltec.com/api",apiUrl:"https://apitienda.smartdigitaltec.com/api",baseUrlImagenes:"https://apitienda.smartdigitaltec.com/assets/"};export{t as a};
--- dist/proy_test/browser/chunk-IYW2DV6Z.js
+++
@@ -1,1 +0,0 @@
-var i={production:!0,urlDominioApi:"https://apicredi.maquimotora.com",urlRaiz:"https://apicredi.maquimotora.com/api",apiUrl:"https://apicredi.maquimotora.com/api",baseUrlImagenes:"https://apicredi.maquimotora.com/assets/"};export{i as a};
